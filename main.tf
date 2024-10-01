e2ab5b380e109e937305c6b8f534a8a7dc8d209c
rajmanda <raj.manda@gmail.com>
2024-09-30 21:46:46 -0400

    changin the branch name to main

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-resource "google_storage_bucket" "my-bucket" {
-  name          = "bkt-demo-000"
+resource "google_storage_bucket" "my-tf-gcp-wif-bucket-01" {
+  name          = "tf-gcp-wif-001"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "tf-gcp-wif"
   force_destroy = true
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "my-bucket2" {
-  name          = "bkt-demo-002"
+resource "google_storage_bucket" "my-tf-gcp-wif-bucket-02" {
+  name          = "tf-gcp-wif-002"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "tf-gcp-wif"
   force_destroy = true
   public_access_prevention = "enforced"
 }
