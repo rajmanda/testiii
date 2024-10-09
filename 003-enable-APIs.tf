0f9c670503efa64649a9347129bf20f557449b6d
rajmanda <raj.manda@gmail.com>
2024-10-09 17:55:41 -0400

    check if the API's are enabled

--- 003-enable-APIs.tf
+++ 003-enable-APIs.tf
@@ -3,32 +3,26 @@
 
 # Data source to check if the Compute Engine API is enabled
 data "google_project_service" "compute" {
-  project = "properties-app-418208"
   service = "compute.googleapis.com"
 }
 
-# Data source to check if the Kubernetes Engine API is enabled
 data "google_project_service" "container" {
-  project = "properties-app-418208"
   service = "container.googleapis.com"
 }
 
-# Enable Compute Engine API if not already enabled
 resource "google_project_service" "compute" {
-  count                   = data.google_project_service.compute ? 0 : 1
-  project                = "properties-app-418208"
-  service                = "compute.googleapis.com"
-  disable_on_destroy     = true
-  disable_dependent_services = true
+  count                   = length(data.google_project_service.compute) == 0 ? 1 : 0
+  service                 = "compute.googleapis.com"
+  disable_on_destroy      = true  # Keeps service enabled even if destroyed
+  disable_dependent_services = true  # Disables dependent services automatically
 }
 
-# Enable Kubernetes Engine API if not already enabled
 resource "google_project_service" "container" {
-  count                   = data.google_project_service.container ? 0 : 1
-  project                = "properties-app-418208"
-  service                = "container.googleapis.com"
-  disable_on_destroy     = true
+  count                   = length(data.google_project_service.container) == 0 ? 1 : 0
+  service                 = "container.googleapis.com"
+  disable_on_destroy      = true  # Keeps service enabled even if destroyed
 }
+
 
 
 
