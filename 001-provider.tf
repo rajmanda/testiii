2bcb0a00eb5176e77f4d104cbeef58a89d272d6b
Raj Manda <raj.manda@gmail.com>
2024-10-12 16:52:26 -0400

    Terraform Create Backend State

--- 001-provider.tf
+++ 001-provider.tf
@@ -2,10 +2,3 @@ provider "google" {
   project = "properties-app-418208"
   region  = "us-central1" # Specify the desired region
 }
-
-terraform {
-  backend "gcs" {
-    bucket  = "tf-gcp-wif-tfstate"
-    prefix  = "terraform/state"   # Optional, used for organization within the bucket
-  }
-}
