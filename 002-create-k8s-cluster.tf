62fd2fbf5eecdd019317f904c1428345f6dedcca
Raj Manda <raj.manda@gmail.com>
2024-10-13 09:20:17 -0400

    disable enable_private_endpoint in your Terraform file

--- 002-create-k8s-cluster.tf
+++ 002-create-k8s-cluster.tf
@@ -6,7 +6,7 @@ resource "google_container_cluster" "primary" {
 
   private_cluster_config {
     enable_private_nodes    = true
-    enable_private_endpoint = true
+    enable_private_endpoint = false
     master_ipv4_cidr_block  = "172.16.0.0/28"  # Define the master CIDR block
   }
 
