019e548f3cfe3572d2a231e8453f91f6cdd814e9
rajmanda <raj.manda@gmail.com>
2024-10-15 17:09:50 -0400

    Removing duplicate providers

--- 004-create-eureka.tf
+++ 004-create-eureka.tf
@@ -7,21 +7,21 @@ data "google_container_cluster" "gke_cluster" {
   location = "us-central1-a"  # Cluster location
 }
 
-# Define the Kubernetes provider
-provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
-}
+# # Define the Kubernetes provider
+# provider "kubernetes" {
+#   host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+#   token                  = data.google_client_config.default.access_token
+#   cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
+# }
 
-# Define the Helm provider
-provider "helm" {
-  kubernetes {
-    host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
-  }
-}
+# # Define the Helm provider
+# provider "helm" {
+#   kubernetes {
+#     host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+#     token                  = data.google_client_config.default.access_token
+#     cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
+#   }
+# }
 
 # Deploy resources on GKE
 resource "kubernetes_namespace" "eureka" {
