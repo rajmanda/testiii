969e315217469b0c52191347fc35112b7cdd239f
rajmanda <raj.manda@gmail.com>
2024-10-15 12:41:03 -0400

    creating kubernetes provider

--- 003-create-nginx-controller.tf
+++ 003-create-nginx-controller.tf
@@ -1,33 +1,45 @@
-# Data source to fetch Google client configuration
-data "google_client_config" "default" {}
-
-# Data source to fetch the Google container cluster
-data "google_container_cluster" "primary" {
-  name     = "gke-cluster"
-  location = "us-central1-a"  # Match the location of your cluster
+# Get the GKE cluster data
+data "google_container_cluster" "gke_cluster" {
+  name     = "gke-cluster"    # Your cluster name
+  location = "us-central1-a"  # Cluster location
 }
 
-# Add the Helm provider
-# provider "helm" {
-#   kubernetes {
-#     cluster_ca_certificate = data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate
-#     host                   = data.google_container_cluster.primary.endpoint
-#     token                  = data.google_client_config.default.access_token  # Use google_client_config for token
-#   }
-# }
+# Define the Kubernetes provider
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
+}
+
+# Define the Helm provider
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    config_path = "~/.kube/config"  # Alternatively, use the Kubernetes provider config
+  }
+}
+# Deploy resources on GKE
+resource "kubernetes_namespace" "example" {
+  metadata {
+    annotations = {
+      name = "ingress-nginx"
+    }
+
+    labels = {
+      mylabel = "ingress-nginx"
+    }
+
+    name = "ingress-nginx"
   }
 }
 
-# Install the NGINX Ingress controller
+# Deploy the NGINX Ingress Controller using Helm
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "bitnami/nginx-ingress-controller"  # Use Bitnami repository
   chart      = "nginx-ingress-controller"
-  version    = "11.4.4"                            # Update this to the latest chart version; get this by running `helm search repo nginx-ingress`
-  namespace  = "kube-system"
+  version    = "11.4.4"                            # Update this to the latest chart version
+  namespace  = "ingress-nginx"
+
   values = [
     <<EOF
 controller:
@@ -38,4 +50,9 @@ controller:
     loadBalancerIP: "34.44.172.58"  # Specify your static external IP here
 EOF
   ]
+}
+
+# Output the NGINX Ingress Controller service endpoint
+output "nginx_ingress_service" {
+  value = helm_release.nginx_ingress.status
 }
