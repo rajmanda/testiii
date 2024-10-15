221dde449f2d19308bdc7368ccf49382208fb3ea
rajmanda <raj.manda@gmail.com>
2024-10-15 17:45:25 -0400

    installing dependency first.

--- 004-create-eureka.tf
+++ 004-create-eureka.tf
@@ -23,21 +23,33 @@ provider "helm" {
   }
 }
 
-# Deploy resources on GKE
+# Deploy resources on GKE - create namespace 
 resource "kubernetes_namespace" "eureka" {
   metadata {
     name = "eureka"
   }
 }
 
-# Deploy the Eureka Server using Helm 
+# Deploy the Bitnami Common Chart first
+resource "helm_release" "common" {
+  name       = "common"
+  repository = "https://charts.bitnami.com/bitnami"  # Bitnami chart repository
+  chart      = "common"                               # Common chart name
+  version    = "2.24.0"                              # Specific version if needed
+
+  namespace  = kubernetes_namespace.eureka.metadata[0].name  # Reference the created namespace
+
+  depends_on = [kubernetes_namespace.eureka]  # Ensure the namespace is created first
+}
+
+# Deploy the Eureka Server using Helm
 resource "helm_release" "eureka" {
   name       = "my-eureka"
   repository = "https://ygqygq2.github.io/charts/"  # Eureka chart repository
   chart      = "eureka"                             # Chart name
   version    = "2.0.0"                              # Specific version
 
-  namespace  = kubernetes_namespace.eureka.metadata[0].name  # Reference the created namespace
+  namespace  = kubernetes_namespace.eureka.metadata[0].name  # Specify the namespace to deploy
 
   set {
     name  = "REGISTER_WITH_EUREKA"
@@ -54,7 +66,7 @@ resource "helm_release" "eureka" {
     value = "False"
   }
 
-  depends_on = [kubernetes_namespace.eureka]  # Ensure the namespace is created before Helm release
+  depends_on = [helm_release.common]  # Ensure common chart is installed before Eureka
 }
 
 # Output the Eureka Helm release status
@@ -65,18 +77,12 @@ output "eureka_helm_release_status" {
 # Data source to fetch Eureka service details
 data "kubernetes_service" "eureka_service" {
   metadata {
-    name      = "my-eureka"  # Change this to the actual Eureka service name
-    namespace = kubernetes_namespace.eureka.metadata[0].name  # Use the created namespace
+    name      = "my-eureka"  # Change this to the actual Eureka service name if needed
+    namespace = kubernetes_namespace.eureka.metadata[0].name  # Adjust the namespace if needed
   }
-
-  depends_on = [helm_release.eureka]  # Ensure the Helm release is completed before querying
 }
 
 # Output the Eureka service LoadBalancer IP (or ClusterIP if applicable)
 output "eureka_service_endpoint" {
   value = data.kubernetes_service.eureka_service.status[0].load_balancer[0].ingress[0].ip
-  description = "LoadBalancer IP of the Eureka service"  # Description for clarity
 }
-
-
-
