d8b713beddf2d6e06fb7d6e0acbb0a53e9966e08
rajmanda <raj.manda@gmail.com>
2024-10-15 11:39:22 -0400

    Adding provider inline

--- 001-provider.tf
+++ 001-provider.tf
@@ -16,12 +16,12 @@ provider "kubernetes" {
   token                  = data.google_client_config.default.access_token
 }
 
-provider "helm" {
-  kubernetes {
-    host                    = google_container_cluster.primary.endpoint
-    cluster_ca_certificate  = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-    token                   = data.google_client_config.default.access_token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                    = google_container_cluster.primary.endpoint
+#     cluster_ca_certificate  = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+#     token                   = data.google_client_config.default.access_token
+#   }
+# }
 
-data "google_client_config" "default" {}
+# data "google_client_config" "default" {}
