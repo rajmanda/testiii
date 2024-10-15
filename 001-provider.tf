06b3d468c40d69f2100881b23ff2afa0f47f0104
rajmanda <raj.manda@gmail.com>
2024-10-15 10:38:52 -0400

    Creating nginx controller from terraform

--- 001-provider.tf
+++ 001-provider.tf
@@ -9,3 +9,19 @@ terraform {
     prefix  = "terraform/state"   # Optional, used for organization within the bucket
   }
 }
+
+provider "kubernetes" {
+  host             = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  token                = data.google_client_config.default.access_token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    cluster_ca_certificate  = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
+}
+
+data "google_client_config" "default" {}
