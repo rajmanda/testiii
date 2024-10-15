242b563e87bf079cc69bb8ccea698c970350f994
rajmanda <raj.manda@gmail.com>
2024-10-15 17:40:13 -0400

    don't have to rouch nginx

--- 004-create-eureka.tf
+++ 004-create-eureka.tf
@@ -1,27 +1,27 @@
-# # Get the Google client configuration
-# data "google_client_config" "default" {}
+# Get the Google client configuration
+data "google_client_config" "default" {}
 
-# # Get the GKE cluster data
-# data "google_container_cluster" "gke_cluster" {
-#   name     = "gke-cluster"    # Your cluster name
-#   location = "us-central1-a"  # Cluster location
-# }
+# Get the GKE cluster data
+data "google_container_cluster" "gke_cluster" {
+  name     = "gke-cluster"    # Your cluster name
+  location = "us-central1-a"  # Cluster location
+}
 
-# # Define the Kubernetes provider
-# provider "kubernetes" {
-#   host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
-#   token                  = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
-# }
+# Define the Kubernetes provider
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
+}
 
-# # Define the Helm provider
-# provider "helm" {
-#   kubernetes {
-#     host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
-#     token                  = data.google_client_config.default.access_token
-#     cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
-#   }
-# }
+# Define the Helm provider
+provider "helm" {
+  kubernetes {
+    host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth.0.cluster_ca_certificate)
+  }
+}
 
 # Deploy resources on GKE
 resource "kubernetes_namespace" "eureka" {
