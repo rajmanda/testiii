30626b167757ddefc19767d0f07584492148a6e9
rajmanda <raj.manda@gmail.com>
2024-10-15 12:28:58 -0400

    removing kubernetes provider

--- 001-provider.tf
+++ 001-provider.tf
@@ -10,11 +10,11 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  token                  = data.google_client_config.default.access_token
-}
+# provider "kubernetes" {
+#   host                   = google_container_cluster.primary.endpoint
+#   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+#   token                  = data.google_client_config.default.access_token
+# }
 
 # provider "helm" {
 #   kubernetes {
