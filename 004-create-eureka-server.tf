75000c06581b983d66fde5e0a67077b27e643e7c
rajmanda <raj.manda@gmail.com>
2024-10-15 20:11:46 -0400

    let it fail

--- 004-create-eureka-server.tf
+++ 004-create-eureka-server.tf
@@ -1,67 +1,67 @@
-# Step 1: Create a directory for the charts first
-resource "null_resource" "prepare_eureka_chart" {
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Creating directory for Eureka charts..."
-      mkdir -p ./eureka/charts
-      echo "Directory './eureka/charts' created successfully."
-    EOT
-  }
-}
+# # Step 1: Create a directory for the charts first
+# resource "null_resource" "prepare_eureka_chart" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       echo "Creating directory for Eureka charts..."
+#       mkdir -p ./eureka/charts
+#       echo "Directory './eureka/charts' created successfully."
+#     EOT
+#   }
+# }
 
-# Step 2: Fetch and extract the charts
-resource "null_resource" "fetch_and_extract_charts" {
-  depends_on = [null_resource.prepare_eureka_chart]
+# # Step 2: Fetch and extract the charts
+# resource "null_resource" "fetch_and_extract_charts" {
+#   depends_on = [null_resource.prepare_eureka_chart]
 
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Fetching Eureka chart..."
-      helm fetch ygqygq2/eureka --version 2.0.0
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       echo "Fetching Eureka chart..."
+#       helm fetch ygqygq2/eureka --version 2.0.0
 
-      echo "Fetching Bitnami common chart..."
-      helm fetch bitnami/common --version 2.26.0
+#       echo "Fetching Bitnami common chart..."
+#       helm fetch bitnami/common --version 2.26.0
 
-      if [ -f eureka-2.0.0.tgz ]; then
-        echo "Extracting Eureka chart..."
-        tar -xvzf eureka-2.0.0.tgz
-        echo "Eureka chart extracted successfully."
-      else
-        echo "Eureka chart not found!"
-        exit 1
-      fi
+#       if [ -f eureka-2.0.0.tgz ]; then
+#         echo "Extracting Eureka chart..."
+#         tar -xvzf eureka-2.0.0.tgz
+#         echo "Eureka chart extracted successfully."
+#       else
+#         echo "Eureka chart not found!"
+#         exit 1
+#       fi
 
-      if [ -f common-2.26.0.tgz ]; then
-        echo "Extracting Common chart..."
-        tar -xvzf common-2.26.0.tgz
-        echo "Common chart extracted successfully."
-      else
-        echo "Common chart not found!"
-        exit 1
-      fi
-    EOT
-  }
-}
+#       if [ -f common-2.26.0.tgz ]; then
+#         echo "Extracting Common chart..."
+#         tar -xvzf common-2.26.0.tgz
+#         echo "Common chart extracted successfully."
+#       else
+#         echo "Common chart not found!"
+#         exit 1
+#       fi
+#     EOT
+#   }
+# }
 
-# Step 3: Move the common chart into the eureka chart's charts/ directory
-resource "null_resource" "move_common_chart" {
-  depends_on = [null_resource.fetch_and_extract_charts]
+# # Step 3: Move the common chart into the eureka chart's charts/ directory
+# resource "null_resource" "move_common_chart" {
+#   depends_on = [null_resource.fetch_and_extract_charts]
 
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Moving common chart into the Eureka charts directory..."
-      if [ -d common ]; then
-        mv common ./eureka/charts/
-        echo "Common chart moved to './eureka/charts/'."
-      else
-        echo "Common directory not found!"
-        exit 1
-      fi
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       echo "Moving common chart into the Eureka charts directory..."
+#       if [ -d common ]; then
+#         mv common ./eureka/charts/
+#         echo "Common chart moved to './eureka/charts/'."
+#       else
+#         echo "Common directory not found!"
+#         exit 1
+#       fi
 
-      echo "Contents of './eureka/charts/':"
-      ls -l ./eureka/charts/
-    EOT
-  }
-}
+#       echo "Contents of './eureka/charts/':"
+#       ls -l ./eureka/charts/
+#     EOT
+#   }
+# }
 
 # Install the Eureka Helm chart
 resource "helm_release" "eureka" {
