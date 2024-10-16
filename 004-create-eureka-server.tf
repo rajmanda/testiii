746e0f4c8da39e33c9494b37fca3c38dfc0641e3
rajmanda <raj.manda@gmail.com>
2024-10-15 20:04:57 -0400

    one more time

--- 004-create-eureka-server.tf
+++ 004-create-eureka-server.tf
@@ -1,44 +1,13 @@
-# # Step 2a: Fetch and extract the bitnami/common charts
-# resource "null_resource" "fetch_and_extract_charts_common" {
-#   depends_on = [null_resource.prepare_eureka_chart]
+# Step 1: Create a directory for the charts first
+resource "null_resource" "prepare_eureka_chart" {
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p ./eureka/charts
+    EOT
+  }
+}
 
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       helm fetch bitnami/common --version 2.26.0
-#       tar -xvzf common-2.26.0.tgz
-#       ls -l
-#     EOT
-#   }
-# }
-
-# # Step 2b: Fetch and extract the charts  - 
-# #Before this 
-#     #helm repo add ygqygq2 https://ygqygq2.github.io/charts/
-#     #helm repo update
-
-# resource "null_resource" "fetch_and_extract_charts_ygqygq2" {
-#   depends_on = [null_resource.fetch_and_extract_charts_common]
-
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       helm fetch ygqygq2/eureka --version 2.0.0
-#       tar -xvzf eureka-2.0.0.tgz
-#       ls -l
-#     EOT
-#   }
-# }
-
-# # Step 3: Move the common chart into the eureka chart's charts/ directory
-# resource "null_resource" "move_common_chart" {
-#   depends_on = [null_resource.fetch_and_extract_charts_common]
-
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       mv common ./eureka/charts/
-#     EOT
-#   }
-# }
-# Step 1: Fetch and extract the charts
+# Step 2: Fetch and extract the charts
 resource "null_resource" "fetch_and_extract_charts" {
   depends_on = [null_resource.prepare_eureka_chart]
 
@@ -46,12 +15,14 @@ resource "null_resource" "fetch_and_extract_charts" {
     command = <<EOT
       helm fetch ygqygq2/eureka --version 2.0.0
       helm fetch bitnami/common --version 2.26.0
+
       if [ -f eureka-2.0.0.tgz ]; then
         tar -xvzf eureka-2.0.0.tgz
       else
         echo "Eureka chart not found!"
         exit 1
       fi
+
       if [ -f common-2.26.0.tgz ]; then
         tar -xvzf common-2.26.0.tgz
       else
@@ -61,17 +32,6 @@ resource "null_resource" "fetch_and_extract_charts" {
     EOT
   }
 }
-# Step 2: Create a directory for the charts
-resource "null_resource" "prepare_eureka_chart" {
-  depends_on = [null_resource.fetch_and_extract_charts]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      mkdir -p ./eureka/charts
-    EOT
-  }
-}
-
 
 # Step 3: Move the common chart into the eureka chart's charts/ directory
 resource "null_resource" "move_common_chart" {
@@ -119,7 +79,6 @@ resource "helm_release" "eureka" {
     value = "1"
   }
 }
-
 
 # Optional: Output Eureka Helm release status
 output "eureka_helm_release_status" {
