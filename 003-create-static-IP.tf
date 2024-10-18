aa8301def9c0bacce403b06ecb0672ffc22eb2f1
Raj Manda <raj.manda@gmail.com>
2024-10-18 07:56:59 -0400

    Creating Static IP for load balancer

---
+++ 003-create-static-IP.tf
@@ -0,0 +1,7 @@
+resource "google_compute_address" "regional_static_ip" {
+  name   = "regional-ngnix-loadbalancer-ip"
+  region = "us-central1"  # Specify the region where you want the static IP
+  lifecycle {
+    prevent_destroy = true  # Prevent Terraform from deleting this resource
+  }
+}
