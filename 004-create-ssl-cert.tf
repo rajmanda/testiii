d0bfc6bc1e9b3e1cefa00c29563a5c3b91ac272f
Raj Manda <raj.manda@gmail.com>
2024-10-18 22:46:46 -0400

    Using terraform module to create RSA

--- 004-create-ssl-cert.tf
+++ 004-create-ssl-cert.tf
@@ -1,53 +1,40 @@
-# Define the Kubernetes namespace
-resource "kubernetes_namespace" "kalyanam" {
-  depends_on = [module.kubernetes-engine_example_simple_autopilot_public]
+# Configure the TLS provider
+provider "tls" {}
+
+# Generate a self-signed TLS certificate
+resource "tls_self_signed_cert" "example" {
+  private_key_pem = file("private_key.pem")
+
+  subject {
+    common_name  = "rajmanda-dev.com"
+    organization = "Rajmanda, LLC"
+  }
+
+  validity_period_hours = 24 * 365
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+# Create a Kubernetes Secret from the generated TLS certificate and key
+resource "kubernetes_secret" "tls_secret" {
   metadata {
-    name = "kalyanam"
-  }
-}
-
-# Create a self-signed certificate using OpenSSL and save the output to specific absolute paths
-resource "null_resource" "generate_certificate" {
-  depends_on = [kubernetes_namespace.kalyanam]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      chmod +x ./generate_certificate.sh
-      "./generate_certificate.sh ./scripts/rajmanda-dev.crt ./scripts/rajmanda-dev.key"
-    EOT
-  }
-
-  # Trigger this block to always run
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-}
-
-# Read the generated certificate from the absolute path
-data "local_file" "tls_cert" {
-  depends_on = [null_resource.generate_certificate]
-  filename   = "./scripts/rajmanda-dev.crt"
-}
-
-# Read the generated private key from the absolute path
-data "local_file" "tls_key" {
-  depends_on = [data.local_file.tls_cert]
-  filename   = "./scripts/rajmanda-dev.key"
-}
-
-# Create a Kubernetes secret to store the TLS certificate and private key
-resource "kubernetes_secret" "rajmanda_dev_tls" {
-  depends_on = [data.local_file.tls_key]
-
-  metadata {
-    name      = "rajmanda-dev-tls"
+    name      = "tls-secret"
     namespace = "kalyanam"
   }
 
   data = {
-    "tls.crt" = base64encode(data.local_file.tls_cert.content)
-    "tls.key" = base64encode(data.local_file.tls_key.content)
+    tls.crt = tls_self_signed_cert.example[0].cert_pem
+    tls.key = tls_self_signed_cert.example[0].private_key_pem
   }
 
   type = "kubernetes.io/tls"
+}
+
+# Output the secret name
+output "secret_name" {
+  value = kubernetes_secret.tls_secret.metadata[0].name
 }
