12c18ad420dd384812597e1dcaac16c37705d19c
Raj Manda <raj.manda@gmail.com>
2024-10-18 22:14:29 -0400

    Updating the script

--- .github/workflows/scripts/generate_certificate.sh
+++ .github/workflows/scripts/generate_certificate.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
 # Check if the key and certificate already exist
-if [[ -f ./rajmanda-dev.key ]]; then
-    echo "Key file already exists:"
-    ls -l ./rajmanda-dev.key
+if [[ -f ./rajmanda-dev.key ]] && [[ -f ./rajmanda-dev.crt ]]; then
+    echo "Key and Certificate already exist."
 else
-    echo "Key file does not exist, will be generated."
+    echo "Generating self-signed certificate and key..."
+    
+    # Generate self-signed certificate using OpenSSL
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+      -keyout ./rajmanda-dev.key \
+      -out ./rajmanda-dev.crt \
+      -subj "/CN=rajmanda-dev.com"
+      
+    echo "Self-signed certificate and key have been generated."
 fi
-
-if [[ -f ./rajmanda-dev.crt ]]; then
-    echo "Certificate file already exists:"
-    ls -l ./rajmanda-dev.crt
-else
-    echo "Certificate file does not exist, will be generated."
-fi
-
-# Generate self-signed certificate using OpenSSL
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout ./rajmanda-dev.key \
-  -out ./rajmanda-dev.crt \
-  -subj "/CN=rajmanda-dev.com"
-
-# Display success message
-echo "Self-signed certificate and key have been generated."
