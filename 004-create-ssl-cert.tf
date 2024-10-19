6a68a795a975106756b9ba56e4bac1a59a80ec36
rajmanda <raj.manda@gmail.com>
2024-10-18 21:28:27 -0400

    Update 004-create-ssl-cert.tf create-nginx-controller

--- 004-create-ssl-cert.tf
+++ 004-create-ssl-cert.tf
@@ -6,21 +6,21 @@ resource "kubernetes_namespace" "kalyanam" {
   }
 }
 
-# Create a self-signed certificate using OpenSSL and save the output to specific absolute paths
+# Create a self-signed certificate using OpenSSL and save the output to the local directory
 resource "null_resource" "generate_certificate" {
   depends_on = [kubernetes_namespace.kalyanam]
-  
-  # Change the absolute path to the desired location
+
   provisioner "local-exec" {
     command = <<EOT
       openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-      -keyout /absolute/path/to/rajmanda-dev.key \
-      -out /absolute/path/to/rajmanda-dev.crt \
+      -keyout ${path.module}/rajmanda-dev.key \
+      -out ${path.module}/rajmanda-dev.crt \
       -subj "/CN=rajmanda-dev.com"
+      chmod 600 /absolute/path/to/rajmanda-dev.key
+      chmod 600 /absolute/path/to/rajmanda-dev.crt
     EOT
   }
 
-  # Trigger this block to always run
   triggers = {
     always_run = "${timestamp()}"
   }
@@ -34,22 +34,22 @@ resource "null_resource" "delay_after_certificate_generation" {
   }
 }
 
-# Read the generated certificate from the absolute path
+# Read the generated certificate
 data "local_file" "tls_cert" {
   depends_on = [null_resource.delay_after_certificate_generation]
-  filename   = "/absolute/path/to/rajmanda-dev.crt"
+  filename   = "${path.module}/rajmanda-dev.crt"
 }
 
-# Read the generated private key from the absolute path
+# Read the generated private key
 data "local_file" "tls_key" {
   depends_on = [data.local_file.tls_cert]
-  filename   = "/absolute/path/to/rajmanda-dev.key"
+  filename   = "${path.module}/rajmanda-dev.key"
 }
 
 # Create a Kubernetes secret to store the TLS certificate and private key
 resource "kubernetes_secret" "rajmanda_dev_tls" {
   depends_on = [data.local_file.tls_key]
-  
+
   metadata {
     name      = "rajmanda-dev-tls"
     namespace = "kalyanam"
