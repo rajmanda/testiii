459b1289192c37c73812c838f23a8fa3b6fdf60b
Raj Manda <raj.manda@gmail.com>
2024-10-19 12:52:52 -0400

    Create Eureka

--- 004-create-eureka-server.tf
+++ 004-create-eureka-server.tf
@@ -1,6 +1,30 @@
+# # Data block to refer to the existing GKE cluster
+data "google_container_cluster" "existing" {
+  name     = "simple-autopilot-public-cluster"  # Replace with your GKE cluster name
+  location = "us-central1"                      # Replace with your cluster location
+}
+
+# Define the Kubernetes provider
+provider "kubernetes" {
+  #host                   = "https://${module.kubernetes-engine_example_simple_autopilot_public.kubernetes_endpoint}"
+  host                   = "https://${data.google_container_cluster.existing.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+# Define the Helm provider
+provider "helm" {
+  kubernetes {
+    #host                   = "https://${module.kubernetes-engine_example_simple_autopilot_public.kubernetes_endpoint}"
+    host                   = "https://${data.google_container_cluster.existing.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
 # Step 1: Create a directory for the charts first
 resource "null_resource" "prepare_eureka_chart" {
-  depends_on = [ google_container_node_pool.primary_nodes ]
+  depends_on = [ data.google_container_cluster.existing ]
   provisioner "local-exec" {
     command = <<EOT
       echo "Creating directory for Eureka charts..."
