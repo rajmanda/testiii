19488e7f0b8a5238bc52a4783d57ef513c9bd51c
Raj Manda <raj.manda@gmail.com>
2024-10-19 10:00:50 -0400

    Create nginx controller

--- 002-create-k8s-cluster.tf
+++ 002-create-k8s-cluster.tf
@@ -1,33 +1,33 @@
-# Creates GKE cluster with name - simple-autopilot-public-cluster
-module "kubernetes-engine_example_simple_autopilot_public" {
-  source  = "terraform-google-modules/kubernetes-engine/google//examples/simple_autopilot_public"
-  version = "33.1.0"
-  project_id = "properties-app-418208"
-}
+# # Creates GKE cluster with name - simple-autopilot-public-cluster
+# module "kubernetes-engine_example_simple_autopilot_public" {
+#   source  = "terraform-google-modules/kubernetes-engine/google//examples/simple_autopilot_public"
+#   version = "33.1.0"
+#   project_id = "properties-app-418208"
+# }
 
-resource "google_compute_firewall" "allow_http" {
-  depends_on = [ module.kubernetes-engine_example_simple_autopilot_public ]
-  name    = "allow-http"
-  network = module.kubernetes-engine_example_simple_autopilot_public.network_name  # Replace with the actual output key if available
+# resource "google_compute_firewall" "allow_http" {
+#   depends_on = [ module.kubernetes-engine_example_simple_autopilot_public ]
+#   name    = "allow-http"
+#   network = module.kubernetes-engine_example_simple_autopilot_public.network_name  # Replace with the actual output key if available
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["http-server"]
-}
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["80"]
+#   }
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags = ["http-server"]
+# }
 
-resource "google_compute_firewall" "allow_https" {
-  depends_on = [ module.kubernetes-engine_example_simple_autopilot_public ]
-  name    = "allow-https"
-  network = module.kubernetes-engine_example_simple_autopilot_public.network_name  # Replace with the actual output key if available
+# resource "google_compute_firewall" "allow_https" {
+#   depends_on = [ module.kubernetes-engine_example_simple_autopilot_public ]
+#   name    = "allow-https"
+#   network = module.kubernetes-engine_example_simple_autopilot_public.network_name  # Replace with the actual output key if available
 
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["443"]
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["https-server"]
-}
+#   source_ranges = ["0.0.0.0/0"]
+#   target_tags = ["https-server"]
+# }
