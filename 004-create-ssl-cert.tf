202a5c5d9adb2f0200e219e2b9ac9c4b805754d7
rajmanda <raj.manda@gmail.com>
2024-10-18 21:03:29 -0400

    Update 004-create-ssl-cert.tf create-nginx-controller

--- 004-create-ssl-cert.tf
+++ 004-create-ssl-cert.tf
@@ -1,5 +1,3 @@
-
-
 # Deploy resources on GKE
 resource "kubernetes_namespace" "kalyanam" {
   depends_on = [module.kubernetes-engine_example_simple_autopilot_public]
@@ -16,11 +14,11 @@ resource "null_resource" "generate_certificate" {
       openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
       -keyout rajmanda-dev.key \
       -out rajmanda-dev.crt \
-      -subj "CN=rajmanda-dev.com"
+      -subj "/CN=rajmanda-dev.com"
     EOT
   }
 
-  #Run this only if the certificate does not already exist
+  # Run this only if the certificate does not already exist
   triggers = {
     always_run = "${timestamp()}"
   }
@@ -29,12 +27,12 @@ resource "null_resource" "generate_certificate" {
 # Read the generated certificate and key from the file
 data "local_file" "tls_cert" {
   depends_on = [null_resource.generate_certificate]  
-  filename = " rajmanda-dev.crt"
+  filename   = "${path.module}/rajmanda-dev.crt"  # Ensure the file path is correct
 }
 
 data "local_file" "tls_key" {
   depends_on = [data.local_file.tls_cert]
-  filename = " rajmanda-dev.key"
+  filename   = "${path.module}/rajmanda-dev.key"  # Ensure the file path is correct
 }
 
 # Create a Kubernetes secret using the generated certificate and key
@@ -47,7 +45,7 @@ resource "kubernetes_secret" "rajmanda_dev_tls" {
 
   data = {
     "tls.crt" = base64encode(data.local_file.tls_cert.content)
-    "tls.key"  = base64encode(data.local_file.tls_key.content)
+    "tls.key" = base64encode(data.local_file.tls_key.content)
   }
 
   type = "kubernetes.io/tls"
