2d87aa9e81129a2599157f265e7902a3a522c469
Raj Manda <raj.manda@gmail.com>
2024-10-22 08:12:02 -0400

    Create Eureka

--- 003-create-eureka-server.tf
+++ 003-create-eureka-server.tf
@@ -31,6 +31,7 @@ provider "helm" {
 # Step 1: Create a directory for the charts first
 resource "null_resource" "prepare_eureka_chart" {
   depends_on = [data.google_container_cluster.existing]
+
   provisioner "local-exec" {
     command = <<EOT
       echo "Creating directory for Eureka charts..."
@@ -48,13 +49,12 @@ resource "null_resource" "fetch_and_extract_charts" {
     command = <<EOT
       echo "Fetching Eureka chart..."
       helm fetch ygqygq2/eureka --version 2.0.0
-
       echo "Fetching Bitnami common chart..."
       helm fetch bitnami/common --version 2.26.0
 
       if [ -f eureka-2.0.0.tgz ]; then
         echo "Extracting Eureka chart..."
-        tar -xvzf eureka-2.0.0.tgz
+        tar -xvzf eureka-2.0.0.tgz -C ./eureka
         echo "Eureka chart extracted successfully."
       else
         echo "Eureka chart not found!"
@@ -63,7 +63,7 @@ resource "null_resource" "fetch_and_extract_charts" {
 
       if [ -f common-2.26.0.tgz ]; then
         echo "Extracting Common chart..."
-        tar -xvzf common-2.26.0.tgz
+        tar -xvzf common-2.26.0.tgz -C ./eureka/charts
         echo "Common chart extracted successfully."
       else
         echo "Common chart not found!"
@@ -73,60 +73,13 @@ resource "null_resource" "fetch_and_extract_charts" {
   }
 }
 
-# Step 3: Move the common chart into the eureka chart's charts/ directory
-resource "null_resource" "move_common_chart" {
-  depends_on = [null_resource.fetch_and_extract_charts]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Moving common chart into the Eureka charts directory..."
-
-      echo ".......RAJ - listing eureka"
-      ls -laR ./eureka
-      
-      echo ".......RAJ - listing common"
-      ls -laR ./common
-
-      if [ -d common ]; then
-        mv common ./eureka/charts/
-        echo "Common chart moved to './eureka/charts/'."
-      else
-        echo "Common directory not found!"
-        exit 1
-      fi
-
-      echo "RAJ ....Contents of './eureka/charts/':"
-      ls -laR ./eureka/charts/
-    EOT
-  }
-}
-
-# Check if the Chart.yaml exists in the Eureka directory
-resource "null_resource" "verify_chart_yaml" {
-  depends_on = [null_resource.move_common_chart]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Verifying the existence of the 'Chart.yaml' file..."
-      if [ ! -f ./eureka/Chart.yaml ]; then
-        echo "'Chart.yaml' file is missing in './eureka' directory!"
-        exit 1
-      else
-        echo "'Chart.yaml' file exists in './eureka' directory."
-      fi
-    EOT
-  }
-}
-
 # Install the Eureka Helm chart
 resource "helm_release" "eureka" {
-  depends_on = [null_resource.verify_chart_yaml, kubernetes_namespace.eurekans]
-  
-  name       = "my-eureka"
-  #chart      = "./eureka"  # Use the local directory after modifying the chart structure
-  chart      = "ygqygq2/eureka"
-  version    = "2.0.0"
+  depends_on = [null_resource.fetch_and_extract_charts]
 
+  name       = "my-eureka"
+  chart      = "./eureka"
+  version    = "2.0.0"
   namespace  = "eureka"
 
   set {
@@ -143,73 +96,14 @@ resource "helm_release" "eureka" {
     name  = "ENABLE_SELF_PRESERVATION"
     value = "False"
   }
-  
+
   set {
     name  = "replicaCount"
     value = "1"
   }
-
-  # Debugging output for Helm release creation
-  provisioner "local-exec" {
-    command = <<EOT
-      echo "Preparing to install the Eureka Helm release..."
-      echo "Checking the contents of the './eureka' directory before installation:"
-      ls -laR ./eureka
-    EOT
-  }
 }
 
-# # Fetch and extract the Eureka Helm chart from Bitnami
-# resource "null_resource" "fetch_and_extract_charts" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       echo "Fetching Eureka chart..."
-#       helm fetch bitnami/eureka --version 8.1.4  # Use Bitnami chart
-
-#       if [ -f eureka-8.1.4.tgz ]; then
-#         echo "Extracting Eureka chart..."
-#         tar -xvzf eureka-8.1.4.tgz
-#         echo "Eureka chart extracted successfully."
-#       else
-#         echo "Eureka chart not found!"
-#         exit 1
-#       fi
-#     EOT
-#   }
-# }
-
-# # Install the Eureka Helm chart using the extracted chart
-# resource "helm_release" "eureka" {
-#   depends_on = [null_resource.fetch_and_extract_charts]
-  
-#   name       = "my-eureka"
-#   chart      = "./eureka"  # Path to the extracted chart directory
-#   version    = "8.1.4"     # Bitnami Eureka chart version
-#   namespace  = "eureka"
-
-#   set {
-#     name  = "replicaCount"
-#     value = "1"
-#   }
-
-#   set {
-#     name  = "REGISTER_WITH_EUREKA"
-#     value = "False"
-#   }
-
-#   set {
-#     name  = "FETCH_REGISTRY"
-#     value = "False"
-#   }
-
-#   set {
-#     name  = "ENABLE_SELF_PRESERVATION"
-#     value = "False"
-#   }
-# }
-
-# Output the Eureka Helm release status
+# Optional: Output Eureka Helm release status
 output "eureka_helm_release_status" {
   value = helm_release.eureka.status
 }
-
