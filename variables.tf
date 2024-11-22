ea265b8e8fe95687755f0bc6c4db4ec9a28a74c0
rajmanda <raj.manda@gmail.com>
2024-11-22 17:17:44 -0500

    Create nginx contoller

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 // variables.tf
-variable "skip_cluster_creation" {
-  description = "Set to true to skip GKE cluster creation"
-  type        = bool
-  default     = false  // Optional: set a default value if desired
-}
+#variable "skip_cluster_creation" {
+#  description = "Set to true to skip GKE cluster creation"
+#  type        = bool
+#  default     = false  // Optional: set a default value if desired
+#}
