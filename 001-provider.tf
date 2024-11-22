6c27be11216d9e686c02ada3af1bc07ad992f2d3
rajmanda <raj.manda@gmail.com>
2024-11-22 15:59:54 -0500

    Update 001-provider.tf

--- 001-provider.tf
+++ 001-provider.tf
@@ -4,15 +4,11 @@ terraform {
     prefix  = "terraform/state"   # Optional, used for organization within the bucket
   }
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.16.1" # Check for the latest version
-    }
     kubernetes = {
       source = "hashicorp/kubernetes"
       version = "2.33.0"
     }
-  }
+ }
 }
 provider "google" {
   project = "properties-app-418208"
